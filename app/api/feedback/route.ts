ff857ffa7d4679ca717123ef33fb58dce7e1fbb3
Robert Angberg <robert.angberg@example.com>
2025-08-25 15:58:28 +0200

    Resend fix

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "../../../auth";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.AUTH_RESEND_KEY);
 
 export async function POST(request: NextRequest) {
   try {
