6abf9570657cfadc8a80d9655fef310cde03180f
Robert Angberg <robert.angberg@example.com>
2025-08-30 23:40:59 +0200

    Fallback bort

--- app/personal/Lonespecar/Forhandsgranskning/Forhandsgranskning/Fotinfo.tsx
+++ app/personal/Lonespecar/Forhandsgranskning/Forhandsgranskning/Fotinfo.tsx
@@ -4,15 +4,20 @@ export default function Fotinfo({ företag }: any) {
   return (
     <div className="mt-auto pt-4 border-t border-gray-400">
       <div className="text-center text-xs text-black">
-        <div className="text-sm font-bold mb-1 text-black">
-          {företag?.företagsnamn || "DITT FÖRETAG AB"}
-        </div>
-        <div>
-          {företag?.adress || "Alléstigen 7B"}, {företag?.postnummer || "72214"}{" "}
-          {företag?.stad || "Västerås"}
-        </div>
-        <div>Org.nr: {företag?.organisationsnummer || "559999-9999"}</div>
-        <div>{företag?.epost || "info@dittforetag.se"}</div>
+        {företag?.företagsnamn && (
+          <div className="text-sm font-bold mb-1 text-black">{företag.företagsnamn}</div>
+        )}
+        {(företag?.adress || företag?.postnummer || företag?.stad) && (
+          <div>
+            {företag?.adress && företag.adress}
+            {företag?.adress && företag?.postnummer && ", "}
+            {företag?.postnummer && företag.postnummer}
+            {företag?.postnummer && företag?.stad && " "}
+            {företag?.stad && företag.stad}
+          </div>
+        )}
+        {företag?.organisationsnummer && <div>Org.nr: {företag.organisationsnummer}</div>}
+        {företag?.epost && <div>{företag.epost}</div>}
         {företag?.telefonnummer && <div>Tel: {företag.telefonnummer}</div>}
       </div>
     </div>
