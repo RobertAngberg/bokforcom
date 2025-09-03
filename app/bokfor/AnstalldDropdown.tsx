1e2b8ce733760494cdc710ed6afd398d0e31c3bf
Robert Angberg <robert.angberg@example.com>
2025-09-03 03:55:14 +0200

    Typefix

--- app/bokfor/AnstalldDropdown.tsx
+++ app/bokfor/AnstalldDropdown.tsx
@@ -1,18 +1,7 @@
 import Dropdown from "../_components/Dropdown";
+import { AnstalldDropdownProps } from "./types";
 
-export interface Anstalld {
-  id: number;
-  förnamn: string;
-  efternamn: string;
-}
-
-interface Props {
-  anstallda: Anstalld[];
-  value: string;
-  onChange: (id: string) => void;
-}
-
-export default function AnstalldDropdown({ anstallda, value, onChange }: Props) {
+export default function AnstalldDropdown({ anstallda, value, onChange }: AnstalldDropdownProps) {
   const options = anstallda.map((a) => ({
     label: `${a.förnamn} ${a.efternamn}`,
     value: a.id.toString(),
