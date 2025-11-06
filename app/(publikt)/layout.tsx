e1f8bf75814238ca5453af4d4f65a6ebb4816712
Robert Angberg <robertangberg@gmail.com>
2025-11-06 03:16:14 +0100

    Speed test 3

--- app/(publikt)/layout.tsx
+++ app/(publikt)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -9,23 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {/* Google tag (gtag.js) */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-1046599495"
-        strategy="afterInteractive"
-      />
-      <Script id="google-ads-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-1046599495');
-        `}
-      </Script>
-
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
