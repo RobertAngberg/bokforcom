e54460cdc5801e2169014d04ff71e316bcd67628
Robert Angberg <robertangberg@gmail.com>
2026-01-14 12:03:10 +0100

    2026 tillagt, startsidan

--- app/start/components/StartClient.tsx
+++ app/start/components/StartClient.tsx
@@ -34,6 +34,7 @@ export default function StartClient({
           value={year}
           onChange={handleYearChange}
           options={[
+            { label: "2026", value: "2026" },
             { label: "2025", value: "2025" },
             { label: "2024", value: "2024" },
             { label: "2023", value: "2023" },
