dfb22dbcface65645bb435647a5838f09956c3f5
Robert Angberg <robertangberg@gmail.com>
2026-02-08 23:04:33 +0100

    Sidebar 4

--- app/_components/MainLayout.tsx
+++ app/_components/MainLayout.tsx
@@ -7,7 +7,9 @@ type Props = {
 export default function MainLayout({ children }: Props) {
   return (
     <main className="min-h-screen bg-cyan-950 overflow-x-hidden text-slate-100">
-      <div className="w-full max-w-5xl mx-auto p-4 md:p-6 lg:p-8">{children}</div>
+      <div className="w-full max-w-5xl mx-auto p-4 md:p-6 lg:p-8">
+        <div className="border border-slate-950/40 p-4 md:p-6 lg:p-8">{children}</div>
+      </div>
       <FeedbackWidget />
     </main>
   );
