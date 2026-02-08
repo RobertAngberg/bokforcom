4796d8b4c8e0004fbb6f287079774e9ec54b7ae2
Robert Angberg <robertangberg@gmail.com>
2026-02-08 23:27:36 +0100

    Fix

--- app/_components/MainLayout.tsx
+++ app/_components/MainLayout.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function MainLayout({ children }: Props) {
   return (
     <main className="min-h-screen bg-slate-950 overflow-x-hidden text-slate-100">
-      <div className="w-full max-w-6xl mx-auto py-3 md:py-5 lg:py-6">
+      <div className="w-full p-3 md:p-5 lg:p-6">
         <div className="bg-cyan-950 p-4 md:p-6 lg:p-8">{children}</div>
       </div>
       <FeedbackWidget />
